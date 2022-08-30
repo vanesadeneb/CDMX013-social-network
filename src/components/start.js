80bd17e0cca4c66084afe82ea22a90ec86f86935
SaumarDev <saumar_1@hotmail.com>
2022-08-30 18:51:23 -0500

    create spa

--- src/components/start.js
+++ src/components/start.js
@@ -1,19 +1,32 @@
-import { onNavigate} from '../main.js';
-const start = () => {
-    const divContainer = document.createElement("div");
-    const logo = document.createElement("img");
-    const profileIcon = document.createElement("img");
-    const loginButton = document.createElement("button"); 
-    const signUpButton = document.createElement("button");
-    const slogan = document.createElement("p");
-    const imgWomenInTech = document.createElement("img");
-    const footer = document.createElement("footer");
+import { onNavigate } from '../main.js';
 
-    logo.src = "../imgs/logo.png";
-    profileIcon.src = "../imgs/profile.png";
-    loginButton.textContent = "Login";
-    signUpButton.textContent = "Sign Up";
-    slogan.textContent = "Empowering Women in Technology";
-    imgWomenInTech.src = "../imgs/imageTech.png";
-    footer.textContent = "2022";
+export const start = () => {
+  const divContainer = document.createElement('div');
+  const logo = document.createElement('img');
+  const profileIcon = document.createElement('img');
+  const loginButton = document.createElement('button');
+  const signUpButton = document.createElement('button');
+  const slogan = document.createElement('p');
+  const imgWomenInTech = document.createElement('img');
+  const footer = document.createElement('footer');
+
+  logo.src = '../imgs/logo.png';
+  profileIcon.src = '../imgs/profile.png';
+  loginButton.textContent = 'Login';
+  signUpButton.textContent = 'Sign Up';
+  slogan.textContent = 'Empowering Women in Technology';
+  imgWomenInTech.src = '../imgs/imageTech.png';
+  footer.textContent = '2022';
+
+  loginButton.addEventListener('click', () => {
+    onNavigate('/login');
+  });
+
+  signUpButton.addEventListener('click', () => {
+    onNavigate('/signUp');
+  });
+
+  divContainer.append(logo, profileIcon, loginButton, signUpButton, slogan, imgWomenInTech, footer);
+
+  return divContainer;
 };
--- src/main.js
+++ src/main.js
@@ -1,31 +1,28 @@
 // Este es el punto de entrada de tu aplicacion
-
-import { loadPartialConfigAsync } from '@babel/core';
-import { myFunction } from './lib/index.js';
 import { start } from './components/start.js';
-import { login } from './components/login.js';
-import { signUp } from './components/sign-up.js';
-import { home } from './components/home.js';
+// import { login } from './components/login.js';
+// import { signUp } from './components/sign-up.js';
+// import { home } from './components/home.js';
 
-
-
-myFunction();
-
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 
 const routes = {
-    '/' : start,
-    '/login' : login,
-    '/sign-up' : signUp,
-    '/home' : home
+  '/': start,
+  // '/login': login,
+  // '/sign-up': signUp,
+  // '/home': home,
 };
 
-const onNavigate = (pathname) => {
-    window.history.pushState(
-        {},
-        pathname,
-        window.location.origin + pathname
-    );
-   root.removeChild(root.firstChild); 
-   root.appendChild(routes[pathname]()); 
-}; 
+export const onNavigate = (pathname) => {
+  window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname,
+  );
+  root.removeChild(root.firstChild);
+  root.appendChild(routes[pathname]());
+};
+
+const path = routes[window.location.pathname];
+
+root.appendChild(path());
