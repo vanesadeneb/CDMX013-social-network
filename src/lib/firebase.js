c7749332aac49d306d55df64e61c9ea016c47c04
menamilochomil <jimenamilochomil@gmail.com>
2022-09-06 11:27:18 -0500

    Adds a new file for firebase issues

---
+++ src/lib/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAP7rv-LIMUMVmMreOEmYNlxC9pSjGKf4g',
+  authDomain: 'women-in-tech-a2721.firebaseapp.com',
+  projectId: 'women-in-tech-a2721',
+  storageBucket: 'women-in-tech-a2721.appspot.com',
+  messagingSenderId: '721813557906',
+  appId: '1:721813557906:web:9c3fde9d14410a535ecc4b',
+  measurementId: 'G-SQTMTQ0BMC',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
