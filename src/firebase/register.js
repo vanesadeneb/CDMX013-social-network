0d5c31578d26744f288c8751459ac1cecb16360e
vanesadeneb <vanessa_diesver@hotmail.com>
2022-09-11 20:07:54 -0700

    modulation

---
+++ src/firebase/register.js
@@ -0,0 +1,11 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+import { app } from '../lib/firebase.js';
+const auth = getAuth(app);
+
+  export const createAccount = async (email, password, confirm) => {
+    return await createUserWithEmailAndPassword(auth, email, password, confirm);
+  };
+
+  export const logIn = async (email, password) => {
+    return await signInWithEmailAndPassword(email, password);
+  }
