f8c394db980a8ab633719afb12219046ecc3ae39
vanesadeneb <vanessa_diesver@hotmail.com>
2022-09-19 19:32:19 -0700

    feat: removes githubLoginButton component

--- src/components/github.js
+++ src/components/github.js
@@ -5,19 +5,6 @@ const githubProvider = new GithubAuthProvider();
 
 const auth = getAuth(app);
 
-export const githubLoginButton = () => {
-    const button = document.createElement('button');
-    const githubIcon = document.createElement('i');
-    button.textContent = 'Log in with Github';
-    button.setAttribute('id', 'githubButton');
-    githubIcon.setAttribute('class', 'fa-brands fa-github');
-
-    button.appendChild(githubIcon);
-
-    
-    return button;
-}
-
 export const githubLogin = async () => {
     const signInGithub = await signInWithPopup(auth, githubProvider);
     return signInGithub;
