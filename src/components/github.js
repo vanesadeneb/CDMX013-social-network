7765a2e6bbbb2a8a3d1c557e395a36b1ea1c6a8c
vanesadeneb <vanessa_diesver@hotmail.com>
2022-09-20 09:05:16 -0700

    refactor: changes popUp firebase method to signInWithRedirect

--- src/components/github.js
+++ src/components/github.js
@@ -1,11 +1,21 @@
-import { getAuth, GithubAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+import { getAuth, GithubAuthProvider, signInWithRedirect, getRedirectResult } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 import { app } from '../lib/firebase.js';
 
 const githubProvider = new GithubAuthProvider();
-
 const auth = getAuth(app);
 
-export const githubLogin = async () => {
-    const signInGithub = await signInWithPopup(auth, githubProvider);
-    return signInGithub;
+signInWithRedirect(auth, githubProvider);
+
+export const githubLogin = () => { 
+getRedirectResult(auth)
+.then((result) => {
+    const credential = GithubAuthProvider.credentialFromResult(result);
+    if (credential) {
+        const token = credential.accessToken;
+      }
+      const user = result.user;
+})
+.catch(err => {
+  console.log(err);
+})
 }
